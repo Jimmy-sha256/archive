0644e367bb158bddc486dc1593c1789824978a3f
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-02-10 17:33:08 +0000

    Update archive_keys.sh

--- archive_keys.sh
+++ archive_keys.sh
@@ -5,21 +5,27 @@
 gpg --import public.gpg
 gpg --import secret.gpg
 
-wget https://github.com/Jimmy-sha256/archive/raw/master/archive.gpg
-
+# set the keys trust level 
 echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key D5557B332830404939C27D578CEDDB5272262D4C trust
 
+# download the encrypted archive
+wget https://github.com/Jimmy-sha256/archive/raw/master/archive.gpg
+
+# decrypt the .gpg
 gpg --decrypt archive.gpg > archive.tar.gz
 
+# securely delete .gpg files
 shred -u -n 33 -z public.gpg
 shred -u -n 33 -z secret.gpg
 shred -u -n 33 -z archive.gpg
 
+# extract archive
 tar xzf archive.tar.gz
 
+# securely delete .tar.gz file
 shred -u -n 33 -z archive.tar.gz
 
-
+# transfer config files from arcive onto host 
 cp /home/jim/archive/config_files/gpg-agent.conf /home/jim/.gnupg
 
 cp /home/jim/archive/config_files/sshcontrol /home/jim/.gnupg
