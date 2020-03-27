7ec4312553c95e943d8857471f59196e25c3ae03
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-03-27 10:12:30 +0000

    Update archive_keys.sh

--- archive_keys.sh
+++ archive_keys.sh
@@ -2,6 +2,13 @@
 
 # copy gpg keys into /home/jim directory before executing script
 
+# update
+
+# install package list
+dpkg --set-selections < ~/archive/package.list
+apt-get dselect-upgrade -y
+
+# import keys
 gpg --import public.gpg
 gpg --import secret.gpg
 
@@ -25,14 +32,21 @@ tar xzf archive.tar.gz
 # securely delete .tar.gz file
 shred -u -n 33 -z archive.tar.gz
 
-# transfer config files from arcive onto host 
-cp /home/jim/archive/config_files/gpg-agent.conf /home/jim/.gnupg
+# transfer config files 
+cp /home/jim/archive/config_files/gpg-agent.conf /home/jim/.gnupg                                                                                          
+cp /home/jim/archive/config_files/sshcontrol /home/jim/.gnupg  
 
-cp /home/jim/archive/config_files/sshcontrol /home/jim/.gnupg
+# transfer dot_files
+cp /home/jim/archive/dot_files/.bash_profile /home/jim/.bash_profile                                                                                                            
+cp /home/jim/archive/dot_files/.bashrc /home/jim/.bashrc                                                                                                                         
+cp /home/jim/archive/dot_files/.gitconfig /home/jim/.gitconfig                                                                                                                  
+cp /home/jim/archive/dot_files/.gitignore_global /home/jim/.gitignore_global                                                                                                    
+cp /home/jim/archive/dot_files/.inputrc /home/jim/.inputrc                                                                                                                      
+cp /home/jim/archive/dot_files/.profile /home/jim/.profile                                                                                        
+cp /home/jim/archive/dot_files/.vimrc /home/jim/.vimrc      
 
-cp /home/jim/archive/config_files/bash_profile /home/jim/.bash_profile
-
-
+# killl gpg agent
 gpgconf --kill gpg-agent
 
+# source bashrc
 source ~/.bash_profile
