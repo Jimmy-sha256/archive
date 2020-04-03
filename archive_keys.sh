de1ef4c62c1203d47472d2bac4affe10907de7e9
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-04-03 22:19:51 +0100

    Update archive_keys.sh

--- archive_keys.sh
+++ archive_keys.sh
@@ -68,6 +68,8 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 35
 
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']" # replace caps with ctrl
+
 # dpkg --get-selections > ~/package.list
 # sudo dpkg --set-selections < ~/archive/package.list
 # sudo apt-get dselect-upgrade -y
