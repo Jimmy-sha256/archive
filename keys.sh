2e44c77a5ac34f874b20a5fdfe02315b6ff15ae0
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-02 11:41:42 +0100

    Create keys.sh

---
+++ keys.sh
@@ -0,0 +1,17 @@
+
+# import keys
+gpg --import secret.gpg
+gpg --import public.gpg
+
+# set the keys trust level 
+echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key D5557B332830404939C27D578CEDDB5272262D4C trust
+
+shred -u -n 33 -z public.gpg
+shred -u -n 33 -z secret.gpg
+
+git clone https://github.com/Jimmy-sha256/config_files.git
+
+# transfer config files
+cp /home/jim/config_files/gpg-agent.conf /home/jim/.gnupg 
+cp /home/jim/config_files/sshcontrol /home/jim/.gnupg 
+cp /home/jim/config_files/gtk.css /home/jim/.config/gtk-3.0
