2a4db692055e0b9653bdd400f9c82fdd9f906ddd
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-02 12:08:35 +0100

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -9,11 +9,25 @@ echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key D5557B332830404939C27
 shred -u -n 33 -z public.gpg
 shred -u -n 33 -z secret.gpg
 
+# download config files / dot files
 git clone https://github.com/Jimmy-sha256/config_files.git
+git clone https://github.com/Jimmy-sha256/dot_files.git
 
 # transfer config files
 cp /home/jim/config_files/gpg-agent.conf /home/jim/.gnupg 
 cp /home/jim/config_files/sshcontrol /home/jim/.gnupg 
 cp /home/jim/config_files/gtk.css /home/jim/.config/gtk-3.0
 
+# transfer dot files
+cp /home/jim/dot_files/.bash_profile /home/jim/
+cp /home/jim/dot_files/.bashrc /home/jim/
+cp /home/jim/dot_files/.gitconfig /home/jim/
+cp /home/jim/dot_files/.gitignore_global /home/jim/
+cp /home/jim/dot_files/.inputrc /home/jim/
+cp /home/jim/dot_files/.profile /home/jim/
+cp /home/jim/dot_files/.tmux.conf /home/jim/
+cp /home/jim/dot_files/.vimrc /home/jim/
+
+# remove temp directories
 rm -rf /home/jim/config_files
+rm -rf /home/jim/dot_files
