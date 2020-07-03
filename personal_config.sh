7501dc870807c88578fc0da7a5aad03c15e2ce76
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-03 19:17:00 +0100

    Delete personal_config.sh

--- personal_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# dock settings
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 35
-
-gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']" # replace caps with ctrl
